b09e250eb4b501ebf5039c649b44855818155526
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-16 00:59:06 +0330

    changed payment method service provider for added extension type

--- src/PaymentMethodServiceProvider.php
+++ src/PaymentMethodServiceProvider.php
@@ -2,9 +2,11 @@
 
 namespace JobMetric\PaymentMethod;
 
-use JobMetric\PackageCore\Exceptions\MigrationFolderNotFoundException;
+use Illuminate\Support\Facades\App;
+use JobMetric\Extension\Facades\ExtensionType;
 use JobMetric\PackageCore\PackageCore;
 use JobMetric\PackageCore\PackageCoreServiceProvider;
+use JobMetric\PaymentMethod\Events\InjectDriverNamespaceEvent;
 
 class PaymentMethodServiceProvider extends PackageCoreServiceProvider
 {
@@ -12,13 +14,28 @@ class PaymentMethodServiceProvider extends PackageCoreServiceProvider
      * @param PackageCore $package
      *
      * @return void
-     * @throws MigrationFolderNotFoundException
      */
     public function configuration(PackageCore $package): void
     {
         $package->name('laravel-payment-method')
             ->hasConfig()
-            ->hasMigration()
             ->hasTranslation();
+    }
+
+    public function afterBootPackage(): void
+    {
+        App::booted(function () {
+            $defaultDriverNamespace = config('payment-method.namespaces');
+
+            $event = new InjectDriverNamespaceEvent;
+            event($event);
+
+            $driverNamespace = array_merge($defaultDriverNamespace, $event->driverNamespace);
+
+            ExtensionType::define('payment-method')
+                ->label('payment-method::base.label')
+                ->description('payment-method::base.description')
+                ->driverNamespace($driverNamespace);
+        });
     }
 }
