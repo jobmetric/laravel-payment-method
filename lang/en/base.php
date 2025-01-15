5abfbfe2126c377fac8ceefe275938a14608de1a
majidmohammadian <majeedmohammadian@gmail.com>
2025-01-16 00:57:24 +0330

    added some keys in language files

---
+++ lang/en/base.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Payment Method Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Payment Method for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "label" => "Payment Method",
+    "description" => "Manage Payment Method",
+
+];
---
+++ lang/fa/base.php
@@ -0,0 +1,18 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Base Payment Method Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Payment Method for
+    | various messages that we need to display to the user.
+    |
+    */
+
+    "label" => "روش های پرداخت",
+    "description" => "مدیریت روش های پرداخت",
+
+];
